a67f6a85e6ea5e50ea8f952729a5d5d7393a7fcf
z <z@test.com>
2022-04-21 02:24:34 +0800

    add

---
+++ sh/network/superspeed.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# 测速工具： https://github.com/oooldking/script
+
+wget -qO- --no-check-certificate https://raw.githubusercontent.com/oooldking/script/master/superbench.sh | bash
+#或者
+# curl -Lso- -no-check-certificate https://raw.githubusercontent.com/oooldking/script/master/superbench.sh | bash
+
+
+# 秋水逸冰大佬的Bench.sh脚本
+
+wget -qO- bench.sh | bash
+#或者
+curl -Lso- bench.sh | bash
+#或者
+wget -qO- 86.re/bench.sh | bash
+#或者
+curl -so- 86.re/bench.sh | bash
+#Github地址：https://github.com/teddysun/across/blob/master/bench.sh
